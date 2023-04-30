393e0165a4522d83990138fb9086572e8ccfef82
mdSofiullah <132186760+mdsofiullah290@users.noreply.github.com>
2023-04-30 12:01:21 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,92 @@
+import React, {useState, useEffect} from "react";
+import Countries from './components/Countries.js';   
+import Search from "./components/Search.js";
+
+const url = "https://restcountries.com/v3.1/all";
+
+function App() {
+ 
+const [isLoading, setIsLoading] = useState(true);
+const [error, setError] = useState(null);
+const [countries, setCountries]= useState([]);
+const [filteredCountries, setFilteredCountries] = useState(countries);
+
+
+const fetchData = async(url)=>{
+  setIsLoading(true);
+
+ try{
+  const response = await fetch(url);
+  const data = await response.json();
+  setCountries(data);
+  setFilteredCountries(data);
+  setIsLoading(false);
+  setError(null);
+  console.log(countries);
+ }catch(error){
+  setIsLoading(false);
+  setError(error);
+ }
+
+
+}
+
+useEffect(()=>{
+
+fetchData(url)
+
+
+},[])
+
+const handleRemoveCountry= (name)=>{
+  // alert(name);
+
+  const filter = filteredCountries.filter((country)=>country.name.common !== name)
+  setFilteredCountries(filter);
+
+}
+
+const handleSearch=(searchValue)=>{
+ let value = searchValue.toLowerCase();
+ const newCountries = countries.filter((country)=>{
+  const countryName = country.name.common.toLowerCase();
+
+  return countryName.startsWith(value)
+ })
+
+ setFilteredCountries(newCountries);
+
+}
+
+
+
+  return (
+    <div>
+
+
+      <h1>Country App</h1>
+      <Search onSearch={handleSearch} />
+      {isLoading && <h2>Loading....</h2>}
+      {error && <h2>{error.message}</h2>}
+      {countries && <Countries 
+      countries={filteredCountries} 
+      onRemoveCountry={handleRemoveCountry} />}
+  
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
